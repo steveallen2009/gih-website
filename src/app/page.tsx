dd87712a977b05a8b7d0cca131b742c8cbadf01d
Steve Allen <steve2002allen@gmail.com>
2026-05-19 13:03:25 +0530

    Add page transition animation to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,17 @@ import LuxuryShowcaseSection from '@/components/sections/LuxuryShowcaseSection'
 import ServicesSection from '@/components/sections/ServicesSection'
 import StatsSection from '@/components/sections/StatsSection'
 import ResortsShowcase from '@/components/sections/ResortsShowcase'
+import PageTransition from '@/components/ui/PageTransition'
 
 export default function HomePage() {
   return (
-    <>
+    <PageTransition>
       <HeroSection />
       <IntroSection />
       <LuxuryShowcaseSection />
       <ServicesSection />
       <StatsSection />
       <ResortsShowcase />
-    </>
+    </PageTransition>
   )
 }
